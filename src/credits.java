abfc8a53f786947b4a5f1f97d435642d6e51ec81
Bobbybob <Bobbybob9900@gmail.com>
2020-05-25 21:41:03 -0400

    yote or yat

---
+++ src/credits.java
@@ -0,0 +1,93 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+
+public class credits {
+    String nextScreen="";
+
+    BufferedImage back,monitor,backButton;
+    Rectangle backButtonRec= new Rectangle(60,60,80,80);
+
+    int Xoffset,Yoffset;
+    boolean mouseOn=true;
+    credits (){
+        try{
+            back = ImageIO.read(new File("res\\menu back.png"));
+            monitor = ImageIO.read(new File("res\\credit monitor.png"));
+            backButton = ImageIO.read(new File("res\\back button.png"));
+        }
+        catch (IOException e){
+            System.out.println("image not found!");
+        }
+    }
+
+    public void start(int lastX, int lastY) {
+        nextScreen="";
+        Xoffset = lastX/4;
+        Yoffset = lastY/4;
+    }
+
+    public String move(){
+
+
+
+
+        return nextScreen;
+    }
+
+    public void paint(Graphics2D thisFrame) {
+
+        thisFrame.drawImage(back,(Xoffset/8)-60,(Yoffset/8)-60,1010,1010,null);
+        thisFrame.drawImage(monitor,(Xoffset/4)+225,50+(Yoffset/4),500,600,null);
+
+        thisFrame.drawImage(backButton,backButtonRec.x,backButtonRec.y,backButtonRec.width,backButtonRec.height,null);
+
+    }
+
+    public void keyTyped(KeyEvent event){
+
+    }
+
+    public void keyReleased(KeyEvent event){
+
+    }
+
+    public void keyPressed(KeyEvent event){
+
+    }
+
+    public void mouseClicked(MouseEvent event){
+        if(backButtonRec.intersects(event.getX(),event.getY(),1,1)){
+            nextScreen="menu";
+        }
+
+    }
+
+    public void mousePressed(MouseEvent event){
+
+    }
+
+    public void mouseReleased(MouseEvent event){
+
+    }
+
+    public void mouseEntered(MouseEvent event){
+        mouseOn=true;
+    }
+
+    public void mouseExited(MouseEvent event){
+        mouseOn=false;
+    }
+
+    public void mouseMoved(MouseEvent event){
+        if(mouseOn){
+            Xoffset = event.getX()/4;
+            Yoffset = event.getY()/4;
+        }
+    }
+}
---
+++ src/screen.java
@@ -0,0 +1,143 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+public class screen {
+    private String screenName;
+    private int lastX,lastY;
+    private menu menu = new menu();
+    private levelSelect levelSelect = new levelSelect();
+    private credits credits = new credits();
+
+    screen(String screenName){
+        this.screenName=screenName;
+    }
+    void changeScreen(String screenName){
+        this.screenName=screenName;
+        switch (screenName){
+            case "menu": menu.start(lastX,lastY);
+            case "levelSelect": levelSelect.start(lastX,lastY);
+            case "credits": credits.start(lastX,lastY);
+        }
+    }
+
+    public String move(){
+        switch (screenName){
+            case "menu": return menu.move();
+            case "levelSelect": return levelSelect.move();
+            case "credits": return credits.move();
+        }
+        return "";
+    }
+
+    public void paint(Graphics2D thisFrame) {
+        switch (screenName){
+            case "menu": menu.paint(thisFrame);
+            break;
+            case "levelSelect": levelSelect.paint(thisFrame);
+            break;
+            case "credits": credits.paint(thisFrame);
+            break;
+        }
+
+    }
+
+    public void keyTyped(KeyEvent event){
+        switch (screenName){
+            case "menu": menu.keyTyped(event);
+            break;
+            case "levelSelect": levelSelect.keyTyped(event);
+            break;
+            case "credits": credits.keyTyped(event);
+            break;
+        }
+    }
+
+    public void keyReleased(KeyEvent event){
+        switch (screenName){
+            case "menu": menu.keyReleased(event);
+            break;
+            case "levelSelect": levelSelect.keyReleased(event);
+            break;
+            case "credits": credits.keyReleased(event);
+            break;
+        }
+    }
+    public void keyPressed(KeyEvent event){
+        switch (screenName){
+            case "menu": menu.keyPressed(event);
+            break;
+            case "levelSelect": levelSelect.keyPressed(event);
+            break;
+            case "credits": credits.keyPressed(event);
+            break;
+        }
+    }
+    public void mouseClicked(MouseEvent event){
+        switch (screenName){
+            case "menu": menu.mouseClicked(event);
+            break;
+            case "levelSelect": levelSelect.mouseClicked(event);
+            break;
+            case "credits": credits.mouseClicked(event);
+            break;
+        }
+    }
+    public void mousePressed(MouseEvent event){
+        switch (screenName){
+            case "menu": menu.mousePressed(event);
+            break;
+            case "levelSelect": levelSelect.mousePressed(event);
+            break;
+            case "credits": credits.mouseClicked(event);
+            break;
+        }
+    }
+    public void mouseReleased(MouseEvent event){
+        switch (screenName){
+            case "menu": menu.mouseReleased(event);
+            break;
+            case "levelSelect": levelSelect.mouseReleased(event);
+            break;
+            case "credits": credits.mouseReleased(event);
+            break;
+        }
+    }
+    public void mouseEntered(MouseEvent event){
+        switch (screenName){
+            case "menu": menu.mouseEntered(event);
+            break;
+            case "levelSelect": levelSelect.mouseEntered(event);
+            break;
+            case "credits": credits.mouseEntered(event);
+            break;
+        }
+    }
+    public void mouseExited(MouseEvent event){
+        switch (screenName){
+            case "menu": menu.mouseExited(event);
+            break;
+            case "levelSelect": levelSelect.mouseExited(event);
+            break;
+            case "credits": credits.mouseExited(event);
+            break;
+        }
+    }
+    public void mouseMoved(MouseEvent event){
+        lastX=event.getX();
+        lastY=event.getY();
+        switch (screenName){
+            case "menu": menu.mouseMoved(event);
+            break;
+            case "levelSelect": levelSelect.mouseMoved(event);
+            break;
+            case "credits": credits.mouseMoved(event);
+            break;
+        }
+    }
+    public int getLastX(){
+        return lastX;
+    }
+    public int getLastY(){
+        return lastY;
+    }
+}
