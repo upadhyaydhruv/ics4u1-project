04ca7acd9e0521270a11b5257a3589f2d126a05b
jgdd11 <johngdion@hotmail.com>
2020-05-27 14:20:55 -0400

    added basic methods to enemy class

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,3 +1,48 @@
 public class Enemy {
-    //testing git
+    private int health;
+    private int xPos;
+    private int yPos;
+    private int xVel;
+    private int yVel;
+    private int mouseX;
+    private int mouseY;
+
+    public Enemy(Keyboard kb, int xOrig, int yOrig){
+        this.xPos = xOrig;
+        this.yPos = yOrig;
+    }
+
+    public Enemy(){
+        //Default constructor required when coding subclass
+    }
+
+    public void decreaseHealth(int differential){
+        health-=differential;
+    }
+
+    public void hit(int x, int y){
+        //TODO
+    }
+
+    public int getHealth(){
+        return this.health;
+    }
+
+    public int getxPos(){
+        return this.xPos;
+    }
+
+    public int getyPos(){
+        return this.yPos;
+    }
+
+    public void setxPos(int xPos){
+        this.xPos = xPos;
+    }
+    public void setYPos(int yPos){
+        this.yPos = yPos;
+    }
+
+    public void move(){
+    }
 }
