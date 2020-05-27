3f951118130cdc381d7fc917ad8fba8da8746362
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-05-27 08:05:47 -0400

    Added player class with necessary methods, fields

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,25 @@
 public class Player {
     private Keyboard keyboard;
-    public Player(Keyboard kb){
+    private int health;
+    private int xPos;
+    private int yPos;
+    private int xVel;
+    private int yVel;
+    private int enemyType;
+    public Player(Keyboard kb, int xOrig, int yOrig){
         keyboard = kb;
+        this.xPos = xOrig;
+        this.yPos = yOrig;
     }
 
-//}
+    public void increaseHealth(int differential){
+        health+=differential;
+    }
+    public void decreaseHealth(int differential){
+        health-=differential;
+    }
+
+    public void hit(int x, int y){
+        //TODO
+    }
+}
