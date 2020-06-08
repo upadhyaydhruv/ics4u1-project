b998f7ccebb96c697a1bbee2fcad78c2390aa5c3
Bobbybob <Bobbybob9900@gmail.com>
2020-06-08 18:55:29 -0400

    bubble particles V1

---
+++ src/BubbleTube.java
@@ -0,0 +1,46 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+public class BubbleTube {
+    int x,y;
+    SmokePart[] smokeParts= new SmokePart[10];
+    BubbleTube(int x,int y){
+        this.x=x;
+        this.y=y;
+        for (int a=0; a<smokeParts.length; a++){
+            smokeParts[a]= new SmokePart(x,y,a*15);
+        }
+    }
+    public BubbleTube() {
+    }
+    static BufferedImage newBubble(){
+        try {
+            switch ((int)(Math.random()*9)){
+                case 1: return ImageIO.read(new File("res\\smoke\\smoke1.png"));
+                case 2: return ImageIO.read(new File("res\\smoke\\smoke2.png"));
+                case 3: return ImageIO.read(new File("res\\smoke\\smoke3.png"));
+                case 4: return ImageIO.read(new File("res\\smoke\\smoke4.png"));
+                case 5: return ImageIO.read(new File("res\\smoke\\smoke5.png"));
+                case 6: return ImageIO.read(new File("res\\smoke\\smoke6.png"));
+                case 7: return ImageIO.read(new File("res\\smoke\\smoke7.png"));
+                case 8: return ImageIO.read(new File("res\\smoke\\smoke8.png"));
+                case 9: return ImageIO.read(new File("res\\smoke\\smoke9.png"));
+            }
+        } catch (IOException e) {
+            System.out.println("image not found!");
+        }
+        return null;
+    }
+    void move() {
+        for (SmokePart smokePart : smokeParts) {
+            smokePart.move();
+        }
+    }
+    void paint(Graphics2D thisFrame) {
+        for (SmokePart smokePart : smokeParts){
+            smokePart.paint(thisFrame);
+        }
+    }
+}
