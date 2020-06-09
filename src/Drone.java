e5f6b8b417d286abcac95a616b76535a06f241e4
jgdd11 <johngdion@hotmail.com>
2020-06-09 19:36:18 -0400

    began working on drone class

---
+++ src/Drone.java
@@ -0,0 +1,32 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Drone extends Enemy{
+        private int x, y, width, height;
+        private int DIAMETER;
+        private BufferedImage image;
+
+        public Drone() {
+            try{
+                image = ImageIO.read(this.getClass().getResource("drone.png"));
+                // image = image.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+                // resizedImage = (BufferedImage) image;
+            } catch(IOException e){
+                System.out.print("there");
+            }
+        }
+
+        public void paint(Graphics2D g2d) {
+        }
+
+        public void move() {
+                if ((x + super.getxVel() < 0) || (x + super.getxVel() > 1020 - DIAMETER)) super.setxVel(super.getyVel()*-1);
+                if ((y + super.getyVel() < 0) || (y + super.getyVel() > 125)) super.setyVel(super.getyVel()*-1);
+
+            x += super.getxVel();
+            y += super.getyVel();
+        }
+}
