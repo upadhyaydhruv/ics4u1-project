edf332934f9c51316b19d08ff2721fcbb4d4add0
Bobbybob <Bobbybob9900@gmail.com>
2020-06-08 23:35:23 -0400

    random optimizations from bobby

--- src/BubbleTube.java
+++ src/BubbleTube.java
@@ -52,3 +52,28 @@ public class BubbleTube {
         Screen.paint(bubbleTubeRec,bubbleTube,thisFrame);
     }
 }
+class BubblePart {
+    int x,y,height,frame;
+    BufferedImage currentBubble;
+    BubblePart(int x, int y,int startHeight) {
+        this.x=x;
+        this.y=y;
+        height=startHeight;
+        currentBubble=BubbleTube.newBubble();
+    }
+    void move(){
+        frame++;
+        if(frame==1000000) {
+            frame=0;
+            height++;
+            if (height == 170) reset();
+        }
+    }
+    void paint(Graphics2D thisFrame){
+        thisFrame.drawImage(currentBubble, x, y-height,44,44, null);
+    }
+    void reset(){
+        height=0;
+        currentBubble=BubbleTube.newBubble();
+    }
+}
--- src/Smoke.java
+++ src/Smoke.java
@@ -13,8 +13,6 @@ public class Smoke {
             smokeParts[a]= new SmokePart(x,y,a*15);
         }
     }
-    public Smoke() {
-    }
     static BufferedImage newSmoke(){
         try {
             switch ((int)(Math.random()*9)){
@@ -42,5 +40,30 @@ public class Smoke {
         for (SmokePart smokePart : smokeParts){
             smokePart.paint(thisFrame);
         }
+    }
+}
+class SmokePart {
+    int x,y,height,frame;
+    BufferedImage currentSmoke;
+    SmokePart(int x, int y,int startHeight) {
+        this.x=x;
+        this.y=y;
+        height=startHeight;
+        currentSmoke=Smoke.newSmoke();
+    }
+    void move(){
+        frame++;
+        if(frame==1000000) {
+            frame=0;
+            height++;
+            if (height > 150) reset();
+        }
+    }
+    void paint(Graphics2D thisFrame){
+        thisFrame.drawImage(currentSmoke, x, y-height,80,80, null);
+    }
+    void reset(){
+        height=0;
+        currentSmoke=Smoke.newSmoke();
     }
 }
