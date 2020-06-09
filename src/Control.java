38b3b359c5ba1f141d3aa6667a16d2b23af9aef9
Bobbybob <Bobbybob9900@gmail.com>
2020-06-08 23:53:33 -0400

    combined keyboard and mouse class into control class

--- src/Control.java
+++ src/Control.java
@@ -1,8 +1,57 @@
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
-import static java.awt.event.MouseEvent.*;
 
-public class Mouse {
+import static java.awt.event.KeyEvent.*;
+
+import static java.awt.event.MouseEvent.*;
+import static java.awt.event.MouseEvent.BUTTON3;
+import static java.lang.Character.toLowerCase;
+class Keyboard {
+    private Boolean W=false,A=false,S=false,D=false,esc=false;
+    public void press(KeyEvent event){
+        switch(event.getKeyCode()){
+            case VK_W: W=true;
+                break;
+            case VK_A: A=true;
+                break;
+            case VK_S: S=true;
+                break;
+            case VK_D: D=true;
+                break;
+            case VK_ESCAPE: esc=true;
+        }
+    }
+    public void release(KeyEvent event){
+        switch(event.getKeyCode()){
+            case VK_W : W=false;
+                break;
+            case VK_A: A=false;
+                break;
+            case VK_S : S=false;
+                break;
+            case VK_D : D=false;
+                break;
+            case VK_ESCAPE: esc=false;
+        }
+    }
+    public Boolean getW(){
+        return W;
+    }
+    public Boolean getA(){
+        return A;
+    }
+    public Boolean getS(){
+        return S;
+    }
+    public Boolean getD(){
+        return D;
+    }
+    public Boolean getEsc(){
+        return esc;
+    }
+}
+class Mouse {
     private int X, Y;
     private boolean LMB,SWB,RMB,mouseOn;
     private MouseEvent event;
@@ -32,11 +81,9 @@ public class Mouse {
     public void entered() {
         mouseOn = true;
     }
-
     public void exited() {
         mouseOn = false;
     }
-
     public int getX(){
         return X;
     }
@@ -57,8 +104,5 @@ public class Mouse {
     }
     public boolean getRMB(){
         return RMB;
-    }
-    public MouseEvent getEvent(){
-        return event;
     }
 }
