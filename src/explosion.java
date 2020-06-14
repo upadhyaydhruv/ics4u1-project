5a083778781bfc2830199fb2eb9e1738ff5c3b9d
Bobbybob <Bobbybob9900@gmail.com>
2020-06-14 15:28:12 -0400

    bulldog fix

--- src/explosion.java
+++ src/explosion.java
@@ -1,9 +1,27 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 public class explosion {
+    BufferedImage pic;
     int[] x = new int[5];
     int[] y = new int[5];
+    explosion(){
+        try {
+            pic = ImageIO.read(Menu.class.getResourceAsStream("menu\\menu back.png"));
+    }catch (IOException e) {
+            System.out.println("image not found!");
+        }
+    }
     public void trigger(int x,int y){
         for(int a=0; a<5; a++){
-            this.x[a]=1;
+            this.x[a]=(int) (Math.random()*50);
+            this.x[a]=(int) (Math.random()*50);
+        }
+    }
+    public void paint(){
+        for(int a=0; a<5; a++){
+
         }
     }
 }
