95c720e4a7dbf8b8e659547a85d5a7311800aff2
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-06-16 15:30:50 -0400

    Added getShape method to HitTable interface

--- src/Hittable.java
+++ src/Hittable.java
@@ -1,8 +1,13 @@
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.PathIterator;
+
 public interface Hittable { // pass an ArrayList<Hittable> of the things relevant to each thing
     class HitBox {
         private boolean round;
         private int x, y, w, h;
-        private int rotateRad; // this assumes rotation about the x-y corner
+        private AffineTransform transform; // this assumes rotation about the x-y corner
 
         public HitBox(boolean round, int w, int h) {
             this.round = round;
@@ -10,13 +15,13 @@ public interface Hittable { // pass an ArrayList<Hittable> of the things relevan
             this.h = h;
         }
 
-        public HitBox(boolean round, int w, int h, int x, int y, int rotateRad) {
+        public HitBox(boolean round, int w, int h, int x, int y, AffineTransform transform) {
             this.round = round;
             this.w = w;
             this.h = h;
             this.x = x;
             this.y = y;
-            this.rotateRad = rotateRad;
+            this.transform = transform;
         }
 
         public void update(int x, int y) {
@@ -26,6 +31,19 @@ public interface Hittable { // pass an ArrayList<Hittable> of the things relevan
 
         public boolean isTouching(HitBox other) {
             throw new RuntimeException("ahlie, dhruv, you messed up again (from John)");
+        }
+
+        private Shape getShape(){
+            if (this.round){
+                Ellipse2D shape = new Ellipse2D.Double(this.x, this.y, this.w, this.h);
+                transform.createTransformedShape(shape).getBounds2D();
+                return shape;
+            }
+            else{
+                Rectangle shape = new Rectangle(this.x, this.y, this.w, this.h);
+                transform.createTransformedShape(shape).getBounds2D();
+                return shape;
+            }
         }
 
         public boolean isWithin(HitBox other) {
