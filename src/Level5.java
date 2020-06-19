4f03ab91d667814c42b0919931d1d00c0ba0b5ed
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-06-18 21:40:57 -0400

    Added level 5

--- src/Level5.java
+++ src/Level5.java
@@ -4,9 +4,11 @@ import java.awt.image.BufferedImage;
 public class Level5 extends Level {
     private BufferedImage water, plat, barrels;
 
-    Rectangle platRec;
-    Rectangle barrelsRec;
-    boolean levelComplete;
+    private Rectangle platRec;
+    private Rectangle barrelsRec;
+    private boolean levelComplete;
+    private int wave = 0;
+    private int ticker = 0;
 
     int[] waveHold = new int[3];
 
@@ -28,13 +30,57 @@ public class Level5 extends Level {
         this.healthBar = new HealthBar(player);
 
         this.addThing(player);
+        this.addBomb();
+        this.addBomb();
+        this.addBomb();
+        this.addBomb();
+        this.addDrone((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+        this.addDrone((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+        this.addDrone((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+        this.addBulldog((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+        this.addBulldog((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+
     }
 
     @Override
     public String moveLevel() {
+        ticker++;
         Screen.waveMove(waveHold);
         if (Main.ENABLE_DEBUG_FEATURES && player.getHealth() == 0)
             System.out.println("player died");
+        if (ticker == 500 && wave != -1) {
+            if (this.countThing(Bulldog.class, Drone.class) == 0) {
+                wave++;
+                if (Main.ENABLE_DEBUG_FEATURES)
+                    System.out.printf("New Wave %d\n", wave);
+                if (wave == 1) {
+                    this.player.setHealth(player.getHealth() + 1);
+                    this.addBomb();
+                    this.addBomb();
+                    this.addBomb();
+                    this.addDrone((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+                    this.addDrone((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+                    this.addBulldog((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+                    this.addBulldog((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+                    this.addBulldog((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+                } else if (wave == 2) {
+                    this.addBomb();
+                    this.player.setHealth(player.getHealth() + 1);
+                    this.addBulldog((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+                    this.addBulldog((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+                    this.addBulldog((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+                    this.addDrone((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+                    this.addDrone((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+                    this.addDrone((int) ((Math.random() * 875) + 1), (int) ((Math.random() * 500) + 1));
+                    this.addBomb();
+                    this.addBomb();
+                    this.addBomb();
+                } else {
+                    wave = -1;
+                }
+            }
+            ticker = 0;
+        }
         return null;
     }
 
@@ -43,6 +89,23 @@ public class Level5 extends Level {
         g.drawImage(water, -60 + waveHold[1], -60 + waveHold[2], 1010, 1010, null);
         Screen.paint(platRec, plat, g);
         Screen.paint(barrelsRec, barrels, g);
+    }
+
+    private void addBulldog(int x, int y) {
+        Bulldog b = new Bulldog(x, y);
+        b.setTarget(player);
+        this.addThing(b);
+    }
+
+    private void addDrone(int x, int y) {
+        Drone d = new Drone(x, y, 1, 1);
+        d.setTarget(player);
+        this.addThing(d);
+    }
+
+    private void addBomb() {
+        Bomb bo = new Bomb();
+        this.addThing(bo);
     }
 
     @Override
